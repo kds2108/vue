fc17f84028c6bf2f6ceb037d21f00ec3b7be4f6e
DariaKuznetsova <kds21082108@gmail.com>
2023-01-18 15:19:21 +0100

    18.01 01

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,10 +4,21 @@ const app = Vue.createApp({
     data() {
         return {
             title: "Hello World",
-            href: "https://www.goodreads.com/"
+            href: "https://www.goodreads.com/",
+            subtitle: "subtitle",
+            obj: {
+                inner: "test"
+            }
         }
     },
-    
+
+    watch: {
+        title(curVal, prevVal) {
+            console.log(curVal, "|", prevVal);
+            this.subtitle = this.title.split(" ").reverse(). join(" ");
+        }
+    },
+
     methods: {
         handleChange(title) {
             this.title = title
